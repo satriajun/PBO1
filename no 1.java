bb9355d254d9175e711cb9246a7cba2b8d2a1e3d
satriajun <114549025+satriajun@users.noreply.github.com>
2022-09-28 09:46:51 +0700

    Create no 1.java

---
+++ no 1.java
@@ -0,0 +1,9 @@
+public class no.1 {
+	   
+	    public static void main(String[] args) {
+	        System.out.println("Belajar JAVA");
+	        System.out.println("Sangat Mudah Sekali");
+	        System.out.println("Dan Sangat Menyenangkan");
+	    }
+	    
+	}
