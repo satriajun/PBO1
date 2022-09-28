f2918fa3f48351b3cc3b0c3e7d39e1eced2dceb5
satriajun <114549025+satriajun@users.noreply.github.com>
2022-09-28 09:47:37 +0700

    Create no 2.java

---
+++ no 2.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+	
+	public class no.2 {
+	
+	  
+	    public static void main(String[] args) {
+	        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+	        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+	    }
+	
+	}
+
